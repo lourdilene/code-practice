14078f1079a49a9e3cdc80992229d0e2209ad182
Lourdilene Souza <lourdilene.souza@gmail.com>
2025-12-26 18:29:16 +0000

    adição de exercício

---
+++ python/functions/list-comprehension-exercise.py
@@ -0,0 +1,7 @@
+users = [
+    {"email": "a@test.com", "active": True},
+    {"email": "b@test.com", "active": False},
+]
+
+# Return something like this:
+# ["a@test.com (active)", "b@test.com (inactive)"]
