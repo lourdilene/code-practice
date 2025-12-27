1e80365c85b686a594bfdb1f68ce53abf896109b
Lourdilene Souza <lourdilene.souza@gmail.com>
2025-12-27 19:15:20 +0000

    adicao de exercicios

---
+++ javascript/map-exercise.js
@@ -0,0 +1,7 @@
+const habits = [
+  { name: 'Meditation', done: true },
+  { name: 'Gym', done: false },
+  { name: 'Study', done: true },
+];
+
+// Resolve using ternary operator and destructuring
--- javascript/map.js
+++ javascript/map.js
@@ -4,11 +4,14 @@ const habits = [
   { name: 'Study', done: true },
 ];
 
-const result = habits.map((habit) => {
-  //   if (habit.done) return 'ok';
-  //   else return 'not';
-  if (habit.done) return habit.name + 'ok';
-  else return habit.name + 'not ok';
-});
+// ternary operator
+const result = habits.map((habit) =>
+  habit.done ? `${habit.name} ok` : `${habit.name} not ok`
+);
+
+// destructuring
+// const result = habits.map(({ name, done }) =>
+//   done ? `${name} ok` : `${name} not ok`
+// );
 
 console.log(result);
